3f53b95617bdb20b8817f9a6d89b167cf12543cb
Tuneer Paul <116104445+TuneerP@users.noreply.github.com>
2026-06-22 02:00:07 +0530

    Delete milestones.js

--- milestones.js
+++
@@ -1,9 +0,0 @@
-import { getSessionFromReq } from "../../../lib/auth";
-
-export default async function handler(req, res) {
-  const session = getSessionFromReq(req);
-  if (!session) {
-    return res.status(200).json({ user: null });
-  }
-  return res.status(200).json({ user: { id: session.sub, username: session.username } });
-}
